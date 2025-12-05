a360fd9dea889c057bb1de60851b10a3e6966b80
rootofpower <kurilyakoleksii12@gmail.com>
2025-12-05 22:42:34 +0100

    implemented REPL

--- src/main.c
+++ src/main.c
@@ -5,12 +5,14 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  printf("$ ");
-
+  
   char command[1024];
-  fgets(command, sizeof(command), stdin);
-  command[strcspn(command, "\n")] = 0; 
 
-  printf("%s: command not found\n", command);
+  while(1) {
+    printf("$ ");
+    fgets(command, sizeof(command), stdin);
+    command[strcspn(command, "\n")] = 0; 
+    printf("%s: command not found\n", command);
+  }
   return 0;
 }
