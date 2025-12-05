4b3f5d78b9b5c9b79fd7b4d75b12c57cb2f848da
rootofpower <kurilyakoleksii12@gmail.com>
2025-12-05 22:43:42 +0100

    implemented exit command

--- src/main.c
+++ src/main.c
@@ -13,6 +13,9 @@ int main(int argc, char *argv[]) {
     fgets(command, sizeof(command), stdin);
     command[strcspn(command, "\n")] = 0; 
     printf("%s: command not found\n", command);
+    if (strcmp(command, "exit") == 0) {
+      break;
+    }
   }
   return 0;
 }
