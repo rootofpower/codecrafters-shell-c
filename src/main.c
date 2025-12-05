cb6a08bef0ddc987a8a4e2e4cb2b31ccb927c526
rootofpower <kurilyakoleksii12@gmail.com>
2025-12-05 22:38:24 +0100

    task 2

--- src/main.c
+++ src/main.c
@@ -5,8 +5,11 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
 
+  char command[1024];
+  fgets(command, sizeof(command), stdin);
+
+  printf("%s: command not found\n", command);
   return 0;
 }
